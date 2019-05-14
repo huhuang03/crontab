66af7b18dca475b7ea8133842c01c3a61b35c0c2
th <huhuang03@gmail.com>
2019-05-14 18:58:24 +0800

    auto commit

--- upAllGit.py
+++ upAllGit.py
@@ -1,46 +1,55 @@
 import os
-import commands
 import datetime
+import subprocess
+import re
 
 rootdir = ["/Users/th/source", "/Users/th"]
 
-print "Begin at " + datetime.datetime.now().strftime('%G-%b-%d %I:%M:%p')
+print("Begin at " + datetime.datetime.now().strftime('%G-%b-%d %I:%M:%p'))
+
+def run_shell(shell):
+    return subprocess.run(shell, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+def is_git_directory(path = '.'):
+    return subprocess.call(['git', '-C', path, 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0
 
 def syncMMDbAndMMXp():
     os.system("rsync -av --delete ~/source/mm-tools/mmdb ~/source/MMdb/")
     os.system("rsync -av --delete ~/source/mm-tools/mmxp ~/source/MMxp/")
 
 def main():
-    syncMMDbAndMMXp()
+    # syncMMDbAndMMXp()
     i = 1
     for rd in rootdir:
         for f in os.listdir(rd):
             path = os.path.join(rd, f)
             if os.path.isdir(path) and os.access(path, os.W_OK):
-                print str(i) + ". deal path: " + path
                 i += 1
                 os.chdir(path)
-                dealpath()
-            print "Done!"
+                dealpath(path)
 
-def dealpath():
-    rst = os.system("git remote -v > /dev/null 2>&1")
-    if rst == 0:
-        output = commands.getstatusoutput("git status")[1]
-        if not "nothing to commit" in output:
-            print "\thas commit"
-            output = commands.getstatusoutput("git remote -v")
-            remote = output[1]
-            if "https://github.com/huhuang03/" in remote or "git@gitlab.com:huhuang03" in remote:
-                os.system("git add .")
-                os.system("git commit -a -m 'auto commit'")
-                os.system("git push")
+def dealpath(path):
+    reg1 = re.compile('origin\s*https://(github|gitlab).com/huhuang03/')
+    reg2 = re.compile('origin\s*git@(github|gitlab).com:huhuang03')
+    if is_git_directory(path):
+        print(f"handel project: {path}")
+        output = run_shell(['git', 'status'])
+        rst = os.system("git remote -v > /dev/null 2>&1")
+        if rst == 0:
+            if not "nothing to commit" in output:
+                print("has commit")
+                remote = run_shell(["git", "remote", '-v'])
+                if reg1.search(remote) or reg2.search(remote):
+                    os.system("git add .")
+                    os.system("git commit -a -m 'auto commit'")
+                    os.system("git push")
+                else:
+                    index = remote.find("\n", 0, len(remote))
+                    remote = remote[0: index]
+                    print("remote is not mine, remote: " + remote)
             else:
-                index = remote.find("\n", 0, len(remote))
-                remote = remote[0: index]
-                print "\tremote is not mine, remote: " + remote
-        else:
-            print "\tnothing to commit"
+                print("nothing to commit")
+        print("Done\n")
 
 if __name__ == "__main__":
     main()
